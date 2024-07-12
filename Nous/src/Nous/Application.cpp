b887e8f9ac72dd066d63179bb5db99a8ef6de168
chordfish <592229466@qq.com>
2024-07-12 23:24:22 +0800

    feat: 顶点缓冲区布局描述 BufferLayout

--- Nous/src/Nous/Application.cpp
+++ Nous/src/Nous/Application.cpp
@@ -10,6 +10,27 @@ namespace Nous {
 
     Application* Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case ShaderDataType::Float:     return GL_FLOAT;
+            case ShaderDataType::Float2:    return GL_FLOAT;
+            case ShaderDataType::Float3:    return GL_FLOAT;
+            case ShaderDataType::Float4:    return GL_FLOAT;
+            case ShaderDataType::Mat3:      return GL_FLOAT;
+            case ShaderDataType::Mat4:      return GL_FLOAT;
+            case ShaderDataType::Int:       return GL_INT;
+            case ShaderDataType::Int2:      return GL_INT;
+            case ShaderDataType::Int3:      return GL_INT;
+            case ShaderDataType::Int4:      return GL_INT;
+            case ShaderDataType::Bool:      return GL_BOOL;
+        }
+
+        NS_CORE_ASSERT(false, "ShaderDataType 未知");
+        return 0;
+    }
+
     Application::Application()
     {
         NS_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -25,18 +46,35 @@ namespace Nous {
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                0.0f,  0.5f, 0.0f
+        float vertices[3 * 7] = {
+                -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+                0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+                0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
         };
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        {
+            BufferLayout layout = {
+                    {ShaderDataType::Float3, "a_Position"},
+                    {ShaderDataType::Float4, "a_Color"}
+            };
+            m_VertexBuffer->SetLayout(layout);
+        }
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        uint32_t index = 0;
+        const BufferLayout& layout = m_VertexBuffer->GetLayout();
+        for (const auto& element : layout)
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index,
+                                  (int) element.GetComponentCount(),
+                                  ShaderDataTypeToOpenGLBaseType(element.Type),
+                                  element.Normalized ? GL_TRUE : GL_FALSE,
+                                  (int) layout.GetStride(),
+                                  reinterpret_cast<const void*>(element.Offset));
+            index++;
+        }
 
         uint32_t indices[3] = { 0, 1, 2 };
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -46,31 +84,31 @@ namespace Nous {
         std::string vertexSrc = R"(
             #version 330 core
 
-            layout(location = 0) in vec3 a_Position;
-//            layout(location = 1) in vec4 a_Color;
-
-            out vec4 gl_Position;
-            out vec4 gl_Color;
-
-            void main()
-            {
-                gl_Position = vec4(a_Position, 1.0);
-//                gl_Color = a_Color;
-                gl_Color = vec4(0.8, 0.2, 0.3, 1.0);
-            }
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+			out vec3 v_Position;
+			out vec4 v_Color;
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = vec4(a_Position, 1.0);
+			}
 
         )";
         std::string fragmentSrc = R"(
             #version 330 core
 
-            in vec4 gl_Color;
+			in vec3 v_Position;
+			in vec4 v_Color;
 
             out vec4 color;
 
-            void main()
-            {
-                color = gl_Color;
-            }
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
 
         )";
         m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
