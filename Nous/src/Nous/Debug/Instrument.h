3cfa309631a2ed572ac42d7c24b44817bcfc664e
chordfish <592229466@qq.com>
2024-07-29 14:06:42 +0800

    adjust: Instrument.h 中 __FUNCSIG__ 宏在不同编译器的自动推断

--- Nous/src/Nous/Debug/Instrument.h
+++ Nous/src/Nous/Debug/Instrument.h
@@ -128,10 +128,30 @@ namespace Nous {
 
 #define NS_PROFILE 1
 #if NS_PROFILE
+// 确定将使用哪个函数签名宏。 这个宏的选择只有在编译器（或预编译器）开始工作时才会被解决。
+// 编辑器中，语法高亮可能会错误地标记了错误的宏。
+    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+        #define NS_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__DMC__) && (__DMC__ >= 0x810)
+        #define NS_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__FUNCSIG__)
+        #define NS_FUNC_SIG __FUNCSIG__
+    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+#define NS_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define NS_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define NS_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define NS_FUNC_SIG __func__
+	#else
+		#define NS_FUNC_SIG "NS_FUNC_SIG unknown!"
+    #endif
+
     #define NS_PROFILE_BEGIN_SESSION(name, filepath) ::Nous::Instrument::Get().BeginSession(name, filepath)
     #define NS_PROFILE_END_SESSION() ::Nous::Instrument::Get().EndSession()
     #define NS_PROFILE_SCOPE(name) ::Nous::InstrumentationTimer timer##__LINE__(name)
-    #define NS_PROFILE_FUNCTION() NS_PROFILE_SCOPE(__FUNCSIG__)
+    #define NS_PROFILE_FUNCTION() NS_PROFILE_SCOPE(NS_FUNC_SIG)
 #else
 #define NS_PROFILE_BEGIN_SESSION(name, filepath)
 	#define NS_PROFILE_END_SESSION()
