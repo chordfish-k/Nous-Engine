e7d838056fbf8e03ca8dc47598a0e81eb9a25480
chordfish <592229466@qq.com>
2024-08-03 14:50:59 +0800

    new:引入entt

---
+++ Nous/src/Nous/Scene/Scene.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+#include "Scene.h"
+
+#include <glm/glm.hpp>
+
+namespace Nous {
+
+    static void OnTransformConstruct()
+    {
+
+    }
+
+    Scene::Scene()
+    {
+        struct MeshComponent
+        {
+            int a;
+        };
+
+        struct TransformComponent
+        {
+            glm::mat4 Transform;
+
+            TransformComponent() = default;
+            TransformComponent(const TransformComponent&) = default;
+            TransformComponent(const glm::mat4& transform)
+                : Transform(transform) {};
+
+            operator glm::mat4&() { return Transform; }
+            operator const glm::mat4&() const { return Transform; }
+        };
+
+        entt::entity entity = m_Registry.create();
+        m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
+
+        m_Registry.on_construct<TransformComponent>().connect<&OnTransformConstruct>();
+
+        if (m_Registry.any_of<TransformComponent>(entity))
+            TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
+
+        // view:找出所有包含某个组件的实体
+        auto view = m_Registry.view<TransformComponent>();
+        for (auto ent : view)
+        {
+            TransformComponent& transform = m_Registry.get<TransformComponent>(ent);
+        }
+
+        auto group = m_Registry.group<TransformComponent>(entt::get<MeshComponent>);
+        for (auto ent : group)
+        {
+            auto[transform, mesh] = group.get<TransformComponent, MeshComponent>(ent);
+        }
+    }
+
+    Scene::~Scene()
+    {
+    }
+}
---
+++ Nous/src/Nous/Scene/Scene.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "entt.hpp"
+
+namespace Nous {
+
+    class Scene
+    {
+    public:
+        Scene();
+        ~Scene();
+    private:
+        entt::registry m_Registry; //注册表=实体上下文，包含所有实体数据
+    };
+
+}
+
