26fe1a812e0b2ffbd597ee63de9e41cf2d82964b
chordfish <592229466@qq.com>
2024-08-04 21:23:04 +0800

    adjust:断言取文件名使用filesystem

--- Nous/src/Nous/Core/Assert.h
+++ Nous/src/Nous/Core/Assert.h
@@ -2,26 +2,12 @@
 
 #include "Nous/Core/Base.h"
 #include "Nous/Core/Log.h"
+#include <filesystem>
 
 #ifdef NS_ENABLE_ASSERTS
-
-namespace Nous::Assert {
-		constexpr const char* CurrentFileName(const char* path) {
-				const char* file = path;
-				while (*path)
-				{
-					if (*path == '/' || *path == '\\')
-						file = ++path;
-					else
-						path++;
-				}
-				return file;
-			}
-	}
-
 	#define NS_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { NS##type##ERROR(msg, __VA_ARGS__); NS_DEBUGBREAK(); } }
 	#define NS_INTERNAL_ASSERT_WITH_MSG(type, check, ...) NS_INTERNAL_ASSERT_IMPL(type, check, "断言失败: {0}", __VA_ARGS__)
-	#define NS_INTERNAL_ASSERT_NO_MSG(type, check) NS_INTERNAL_ASSERT_IMPL(type, check, "断言 '{0}' 失败于 {1}:{2}", NS_STRINGIFY_MACRO(check), ::Nous::Assert::CurrentFileName(__FILE__), __LINE__)
+	#define NS_INTERNAL_ASSERT_NO_MSG(type, check) NS_INTERNAL_ASSERT_IMPL(type, check, "断言 '{0}' 失败于 {1}:{2}", NS_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 	#define NS_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define NS_INTERNAL_ASSERT_GET_MACRO(...) NS_EXPAND_MACRO( NS_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, NS_INTERNAL_ASSERT_WITH_MSG, NS_INTERNAL_ASSERT_NO_MSG) )
