e0d7d7e11dad060f6f3a687c5b36aa5322d66834
chordfish <592229466@qq.com>
2024-08-08 01:55:16 +0800

    new:UniformBuffer

--- Nous/src/Nous/Renderer/UniformBuffer.h
+++ Nous/src/Nous/Renderer/UniformBuffer.h
@@ -6,6 +6,7 @@ namespace Nous {
 
     class UniformBuffer
     {
+    public:
         virtual ~UniformBuffer() {}
         virtual void SetData(const void* data, uint32_t size, uint32_t offset = 0) = 0;
 
